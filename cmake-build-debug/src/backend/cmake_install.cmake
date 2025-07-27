80d75d3fbb25c3ca3edf001d9221b9bb2d893e52
philippeysl20 <rd57yh@student.aau.dk>
2025-07-28 00:44:34 +0200

    Update project configuration for Windows toolchain
    Switched CMake and build configuration from macOS to Windows toolchain paths and executables. Updated .idea workspace settings and cache files to reflect new environment and tool locations.

--- cmake-build-debug/src/backend/cmake_install.cmake
+++ cmake-build-debug/src/backend/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /Users/philippebourrachot/Documents/GitHub/Real-Time-System-Monitor-Dashboard/src/backend
+# Install script for directory: C:/Users/phili/Documents/GitHub/Real-Time-System-Monitor-Dashboard/src/backend
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Real-Time-System-Monitor-Dashboard")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,6 +34,6 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/Library/Developer/CommandLineTools/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2024.2.1/bin/mingw/bin/objdump.exe")
 endif()
 
